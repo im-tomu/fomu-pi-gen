39180b156215fb1ede02e0ba2364f610128168a8
Sean Cross <sean@xobs.io>
2019-03-20 00:05:03 +0800

    config: store image name in config
    Signed-off-by: Sean Cross <sean@xobs.io>

--- run-build-docker.sh
+++ run-build-docker.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-extra_config="extra-config"
+extra_config="config"
 
-source ./config
+sed -i '/IMG_VERSION=/d' config
+sed -i '/IMG_FILENAME=/d' config
+sed -i '/ZIP_FILENAME=/d' config
 
 IMG_VERSION="$(git describe --tags)"
 
-echo "IMG_VERSION=${IMG_VERSION}" > "${extra_config}"
+echo "IMG_VERSION=${IMG_VERSION}" >> "${extra_config}"
 echo "IMG_FILENAME=${IMG_NAME}-${IMG_VERSION}" >> "${extra_config}"
 echo "ZIP_FILENAME=${IMG_NAME}-${IMG_VERSION}" >> "${extra_config}"
 
-./build-docker.sh -c "${extra_config}" 2>&1 | tee build.log
+./build-docker.sh 2>&1 | tee build.log
+git checkout config
