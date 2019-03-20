3898e322c942a7521291cd261f04f75c26347b21
Sean Cross <sean@xobs.io>
2019-03-20 09:17:45 +0800

    run-build-docker: set and source image filename
    Signed-off-by: Sean Cross <sean@xobs.io>

--- run-build-docker.sh
+++ run-build-docker.sh
@@ -8,9 +8,10 @@ sed -i '/ZIP_FILENAME=/d' config
 
 IMG_VERSION="$(git describe --tags)"
 
+source ./config
 echo "IMG_VERSION=${IMG_VERSION}" >> "${extra_config}"
 echo "IMG_FILENAME=${IMG_NAME}-${IMG_VERSION}" >> "${extra_config}"
 echo "ZIP_FILENAME=${IMG_NAME}-${IMG_VERSION}" >> "${extra_config}"
+source ./config
 
 ./build-docker.sh 2>&1 | tee build.log
-git checkout config
